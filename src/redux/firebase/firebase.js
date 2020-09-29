891b79f04a731053fb104187f66006513c3392bd
Amadou <xalima99@gmail.com>
2020-09-29 20:01:57 +0000

    fixed reload on profile img changed

--- src/redux/firebase/firebase.js
+++ src/redux/firebase/firebase.js
@@ -1,22 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1-bm7j5eqp6Hyde8uh2SiW3fB2QynuN4",
-  authDomain: "social-1b384.firebaseapp.com",
-  databaseURL: "https://social-1b384.firebaseio.com",
-  projectId: "social-1b384",
-  storageBucket: "social-1b384.appspot.com",
-  messagingSenderId: "1041197642959",
-  appId: "1:1041197642959:web:339d1765a5148e754cf3ba",
-  measurementId: "G-7685RRFNVD"
-  // apiKey: "AIzaSyDlaLH8esVm2fLCzXgDY0OC7xa24YqHLMc",
-  // authDomain: "evernoter-91b6d.firebaseapp.com",
-  // databaseURL: "https://evernoter-91b6d.firebaseio.com",
-  // projectId: "evernoter-91b6d",
-  // storageBucket: "evernoter-91b6d.appspot.com",
-  // messagingSenderId: "3485284622",
-  // appId: "1:3485284622:web:3f480d2ccc0c44ca587f5f",
-  // measurementId: "G-5E0CMSJNSS",
+  
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
